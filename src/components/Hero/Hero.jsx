c8f25a09dfff511783d5c8f7e534ec09236548c5
Faithkebenei <faithkebs16@gmail.com>
2024-01-06 01:38:27 +0300

    Hero UI

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import text from "../../utils/texts";
 import styles from "./Hero.module.scss";
 import { FaArrowRight } from "react-icons/fa";
@@ -13,6 +13,9 @@ import four from "../../utils/images/4.png";
 import twelve from "../../utils/images/12.png";
 
 const Hero = () => {
+  const [activeImgCount, setActiveImgCount] = useState(1);
+  const [activePhoto, setActivePhoto] = useState(five);
+
   return (
     <div className={styles.hero}>
       <div className={styles.top}>
@@ -30,14 +33,74 @@ const Hero = () => {
           <div className={styles.imageContainer}>
             <div className={styles.blockColor}></div>
             <div className={styles.imgContainer}>
-              <img src={five} alt="Product" />
+              <img src={activePhoto} alt="Product" />
             </div>
             <div className={styles.imgCount}>
-              <p>01</p>
-              <p>02</p>
-              <p>03</p>
-              <p>04</p>
-              <p>05</p>
+              <p
+                style={
+                  activeImgCount === 1
+                    ? { borderBottom: "2px solid #6e9257", color: "#6e9257" }
+                    : {}
+                }
+                onClick={() => {
+                  setActiveImgCount(1);
+                  setActivePhoto(five);
+                }}
+              >
+                01
+              </p>
+              <p
+                style={
+                  activeImgCount === 2
+                    ? { borderBottom: "2px solid #6e9257", color: "#6e9257" }
+                    : {}
+                }
+                onClick={() => {
+                  setActiveImgCount(2);
+                  setActivePhoto(eleven);
+                }}
+              >
+                02
+              </p>
+              <p
+                style={
+                  activeImgCount === 3
+                    ? { borderBottom: "2px solid #6e9257", color: "#6e9257" }
+                    : {}
+                }
+                onClick={() => {
+                  setActiveImgCount(3);
+                  setActivePhoto(six);
+                }}
+              >
+                03
+              </p>
+              <p
+                style={
+                  activeImgCount === 4
+                    ? { borderBottom: "2px solid #6e9257", color: "#6e9257" }
+                    : {}
+                }
+                onClick={() => {
+                  setActiveImgCount(4);
+                  setActivePhoto(four);
+                }}
+              >
+                04
+              </p>
+              <p
+                style={
+                  activeImgCount === 5
+                    ? { borderBottom: "2px solid #6e9257", color: "#6e9257" }
+                    : {}
+                }
+                onClick={() => {
+                  setActiveImgCount(5);
+                  setActivePhoto(twelve);
+                }}
+              >
+                05
+              </p>
             </div>
           </div>
           <div className={styles.socials}>
