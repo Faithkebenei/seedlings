5daf0fb145a1dac3388341efadd695b3f7cf72c9
Faithkebenei <faithkebs16@gmail.com>
2024-01-29 14:51:36 +0300

    Hero section ui

--- src/components/CTO/CTO.jsx
+++ src/components/CTO/CTO.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./CTO.module.scss";
 
+import img from "../../utils/images/7.jpg";
+
 const CTO = () => {
-  return <div className={styles.CTO}></div>;
+  return (
+    <div className={styles.CTO}>
+      <div className={styles.imageContainer}>
+        <img src={img} alt="" />
+        <div className={styles.overlay}></div>
+        <p>Call 0715 XXX XXX Today To Order</p>
+      </div>
+    </div>
+  );
 };
 
 export default CTO;
